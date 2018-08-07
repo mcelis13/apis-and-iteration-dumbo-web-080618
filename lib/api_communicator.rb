9af55fd1479d9b437f4bec56b15293839e94618d
Madeline Celis <celiscb@vcu.edu>
2018-08-07 17:12:38 -0400

    complete

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,29 +1,59 @@
+# note: in this demonstration we name many of the variables _hash or _array.
+# This is done for educational purposes. This is not typically done in code.
+
+
+# iterate over the response hash to find the collection of `films` for the given
+#   `character`
+# collect those film API urls, make a web request to each URL to get the info
+#  for that film
+# return value of this method should be collection of info about each film.
+#  i.e. an array of hashes in which each hash reps a given film
+# this collection will be the argument given to `parse_character_movies`
+#  and that method will do some nice presentation stuff: puts out a list
+#  of movies by title. play around with puts out other info about a given film.
 require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+def fetch(url)
+  response_string = RestClient.get(url)
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
-
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
+def all_links
+  link_array = []
+  link = 'http://www.swapi.co/api/people/?page='
+  for counter in 1..9
+    fetched_url = fetch(link + "#{counter}")
+    link_array << fetched_url['results']
+  end
+  link_array
+end
+
+
+def get_character_movies_from_api(character)
+  films_array = []
+  films_hash = []
+  #get the array with the names and movies
+  all_links.flatten.each do |characterObj|
+    if characterObj['name'].downcase == character
+      films_array = characterObj['films']
+    end
+  end
+
+  films_array.each do |film_link|
+    films_hash << fetch(film_link)
+  end
+
+  films_hash
+end
+
+
 def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  # some iteration magic and puts out the movies in a nice list]
+  films_hash.each_with_index do |movie, idx|
+    puts "#{idx + 1}. #{movie['title']}"
+  end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,12 @@
+
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to SWAMPI finder"
+  puts
 end
 
 def get_character_from_user
   puts "please enter a character"
+  user_input = gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
